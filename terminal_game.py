6068f498f20755159d88f34f2b036b674edc0b4a
WLRSD <lyrawdev@gmail.com>
2025-04-24 15:48:23 -0300

    aprendendo a mexer com modulorização com os arquivos mod_soma e terminal_game

--- terminal_game.py
+++ terminal_game.py
@@ -1,4 +1,7 @@
 from mod_soma import add as somaai
+import wikipedia
 
+summary = wikipedia.summary("Python (programming language)", sentences=2)
+print(summary)
 vamosver = somaai(2,4)
 print(vamosver)
